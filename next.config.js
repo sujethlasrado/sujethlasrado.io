3bbf6251f68e6da812e3390b78a4d3aef1dd6a3f
Sujeth <sujethlasrado8@gmail.com>
2025-10-25 16:42:17 +0200

    secondary next  update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
-module.exports = {
-    swcMinify: true,
-    async redirects() {
-        return [
-          {
-            source: '/blog',
-            destination: '/',
-            permanent: true,
-          },
-        ]
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: true,
       },
+    ];
+  },
+  experimental: {
+    esmExternals: true, // helps with MDX and Contentlayer packages
+  },
 };
+
+module.exports = nextConfig;
