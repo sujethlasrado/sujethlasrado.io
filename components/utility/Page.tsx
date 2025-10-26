1b8b8399ba00323a231c29b2810db87c96662f63
Sujeth <sujethlasrado8@gmail.com>
2025-10-26 23:07:52 +0100

    no prview

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -20,54 +20,30 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
         <title>{pageTitle}</title>
 
         {/* Favicon for browser tab */}
-        {/* <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/static/favicon/apple-touch-icon.png"
-        /> */}
-        {/* <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/static/favicon/favicon-32x32.png"
-        /> */}
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/static/favicon/favicon-16x16.png"
+          href="/static/favicon/android-chrome-192x192.png"
         />
-        {/* <link rel="manifest" href="/static/favicon/site.webmanifest" /> */}
 
         {/* Meta */}
         <meta name="title" content={pageTitle} />
         <meta name="description" content={desc} />
 
-        {/* Open Graph / LinkedIn Preview */}
+        {/* Open Graph / LinkedIn Preview (disabled image for now) */}
         <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://sujethlasrado-io.netlify.app/"
-        />
+        <meta property="og:url" content="https://sujethlasrado-io.netlify.app/" />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={desc} />
-        {/* <meta
-          property="og:image"
-          content="https://sujethlasrado-io.netlify.app/static/favicon/android-chrome-192x192.png"
-        /> */}
+        {/* <meta property="og:image" content="/static/favicon/android-chrome-192x192.png" /> */}
 
-        {/* Twitter Preview
+        {/* Twitter Preview (disabled image for now) */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://sujethlasrado-io.netlify.app/"
-        /> */}
-        {/* <meta property="twitter:title" content={pageTitle} />
+        <meta property="twitter:url" content="https://sujethlasrado-io.netlify.app/" />
+        <meta property="twitter:title" content={pageTitle} />
         <meta property="twitter:description" content={desc} />
-        <meta
-          property="twitter:image"
-          content="https://sujethlasrado-io.netlify.app/static/favicon/android-chrome-192x192.png"
-        /> */}
+        {/* <meta property="twitter:image" content="/static/favicon/android-chrome-192x192.png" /> */}
 
         {/* Google Analytics */}
         <script
