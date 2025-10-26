c5212c80fe1ab53819dab4b691519d1fbc5f62c4
Sujeth <sujethlasrado8@gmail.com>
2025-10-26 21:55:27 +0100

    changed OG image

--- components/designs/More.tsx
+++ components/designs/More.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function More() {
-    return (
-      <p className="py-20 text-sm italic text-fun-gray">
-        That's not all... I've got more on <a className="text-fun-pink underline" href="https://dribbble.com/braydentw" target="_blank">my Dribbble</a>!
-      </p>
-    );
+  return (
+    <p className="py-20 text-sm italic text-fun-gray">
+      That's not all... I've got more on{" "}
+      <a
+        className="text-fun-pink underline"
+        href="https://github.com/sujethlasrado"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        my GitHub
+      </a>
+      !
+    </p>
+  );
 }
 
-export default More
+export default More;
--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -41,21 +41,21 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
         <meta name="description" content={desc} />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://braydentw.io/" />
+        <meta property="og:url" content="https://sujethlasrado-io/" />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={desc} />
         <meta
           property="og:image"
-          content="https://braydentw.io/static/misc/og.png"
+          content="https://sujethlasrado-io/static/misc/og.png"
         />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://braydentw.io/" />
+        <meta property="twitter:url" content="https://sujethlasrado-io/" />
         <meta property="twitter:title" content={pageTitle} />
         <meta property="twitter:description" content={desc} />
         <meta
           property="twitter:image"
-          content="https://braydentw.io/static/misc/og.png"
+          content="https://sujethlasrado-io/static/misc/og.png"
         ></meta>
         <script
           async
